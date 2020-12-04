161d4feefe3dedc89e051455e3b15e274f667c29
Robin Csutorás <csutorasr@gmail.com>
2020-12-04 21:58:58 +0100

    chore: add relay test

--- apps/api/src/app/orangepi/relay.ts
+++ apps/api/src/app/orangepi/relay.ts
@@ -1,16 +1,21 @@
 import { access, constants, writeFile } from 'fs';
 import { promisify } from 'util';
+import { environment } from '../../environments/environment';
 
 const accessAsync = promisify(access);
 const writeFileAsync = promisify(writeFile);
 
 const gpioPath = '/sys/class/gpio/gpio';
 async function setGPIO(port: number, value: boolean) {
+  if (!environment.production) {
+    console.log(`Turn port ${port} ${value ? 'on' : 'off'}.`);
+    return;
+  }
   const portValuePath = `${gpioPath}${port}/value`;
   try {
     await accessAsync(portValuePath, constants.W_OK);
   } catch {
-    throw new Error(`Port ${port} value is not writable for runner user`);
+    throw new Error(`Port ${port} value is not writable for runner user.`);
   }
   await writeFileAsync(portValuePath, value ? '1' : '0');
 }
