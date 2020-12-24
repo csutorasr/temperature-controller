bb171c930b1cc5c7785bc7164d9eefe8b36de514
Robin Csutorás <robin.csutoras@supercharge.io>
2020-12-24 01:53:26 +0100

    chore: update build script for mac os x

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-nx build api --configuration=production
-nx build temperature-controller --configuration=production
+yarn nx build api --configuration=production
+yarn nx build temperature-controller --configuration=production
 mv dist/apps/temperature-controller/* dist/apps/api/assets
